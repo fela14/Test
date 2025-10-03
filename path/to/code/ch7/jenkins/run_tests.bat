1dcca2ab8289be6e9ab5a6a2c2bff0cf05f2808b
fela <k.fakeye@yahoo.com>
2025-10-03 10:39:08 +0000

    install proj corrected

--- path/to/code/ch7/jenkins/run_tests.bat
+++ path/to/code/ch7/jenkins/run_tests.bat
@@ -5,16 +5,15 @@ REM ================================
 
 REM Paths relative to workspace
 set tasks_proj_dir=%WORKSPACE%\path\to\code\ch7\tasks_proj_v2
-set start_tests_dir=%WORKSPACE%\path\to\code\ch7\tasks_proj_v2\tests
+set start_tests_dir=%tasks_proj_dir%\tests
 set results_file=%WORKSPACE%\results.xml
 
 REM Install project
-py -m pip install -e %tasks_proj_dir%
+"C:\Users\Opeyemi\AppData\Local\Programs\Python\Python312\python.exe" -m pip install -e %tasks_proj_dir%
 
 REM Run tests
 cd %start_tests_dir%
-py -m pytest --junit-xml=%results_file%
+"C:\Users\Opeyemi\AppData\Local\Programs\Python\Python312\python.exe" -m pytest --junit-xml=%results_file%
 
 echo Tests complete.
 exit /b %errorlevel%
-
